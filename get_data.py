d35d75f279495d12219342647203b0c75521adb1
Aaron Schumacher <ajschumacher@gmail.com>
2014-02-27 13:13:58 +0000

    attempt to loop around errors

--- get_data.py
+++ get_data.py
@@ -2,12 +2,22 @@
 
 from config import *
 from TwitterAPI import TwitterAPI
+import time
 
-api = TwitterAPI(consumer_key, consumer_secret,
-                 access_token_key, access_token_secret)
+delay = 8 # seconds
 
-r = api.request('statuses/filter', {'track':'data'})
-
-with open(file_location, "a") as output:
-    for item in r.get_iterator():
-        output.write(str(item) + "\n")
+while True:
+    try:
+        api = TwitterAPI(consumer_key, consumer_secret,
+                         access_token_key, access_token_secret)
+        r = api.request('statuses/filter', {'track':'data'})
+        with open(file_location, "a") as output:
+            for item in r.get_iterator():
+                output.write(str(item) + "\n")
+            delay = max(8, delay/2)
+    except:
+        print "Error"
+        print time.ctime()
+        print "Waiting " + str(delay) + " seconds"
+        time.sleep(delay)
+        delay *= 2
